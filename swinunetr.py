9d4f1340e74e940d9715ce5c0ea2b6282b31c195
JF <maestratijf@gmail.com>
2024-04-04 14:46:02 +0200

    d

--- swinunetr.py
+++ swinunetr.py
@@ -32,7 +32,7 @@ from monai.transforms import (
     ToTensord,Orientationd, 
     MaskIntensityd,
     Transform,
-    AddChanneld,
+    EnsureChannelFirstd,
 )
 
 from monai.config import print_config
@@ -153,7 +153,7 @@ print("Using pretrained self-supervied Swin UNETR backbone weights !")
 transforms = Compose([
     LoadImaged(keys=["image", "roi"]),
     DebugTransform(),
-    AddChanneld(keys=["image", "roi"]),
+    EnsureChannelFirstd(keys=["image", "roi"]),
     CropOnROId(keys=["image"], roi_key="roi",size=target_size), 
     DebugTransform(),  # Check the shape right after resizing
     #MaskIntensityd(keys=["image"], mask_key="roi"),
