2930798c75f4f1966ba880c5dc50bc633137c537
JF <maestratijf@gmail.com>
2024-04-09 15:38:28 +0200

    svm classifier

---
+++ svm_classif.py
@@ -0,0 +1,19 @@
+from sklearn import svm
+from mlp_classif import load_data
+
+
+def train_classifier(data_path):
+    x_train, y_train, x_val, y_val,cw = load_data(data_path)
+    clf = svm.LinearSVC()
+    clf.fit(x_train, y_train)
+    
+    accuracy = clf.score(x_val, y_val)
+    print(f"Accuracy sur l'ensemble de validation: {accuracy * 100:.2f}%")
+
+    return clf, accuracy
+
+def main():
+    train_classifier('data/output/features.csv')
+
+if __name__ == '__main__':
+    main()
