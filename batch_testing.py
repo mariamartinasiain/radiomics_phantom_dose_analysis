a947cb5811ba8c01baf7e1379816c189b18e6171
JF <maestratijf@gmail.com>
2024-04-15 11:36:29 +0200

    more classification test

--- batch_testing.py
+++ batch_testing.py
@@ -17,9 +17,9 @@ configurations = {
 }
 classif_type = 'roi_small'
 for model, info_list in configurations.items():
-    print(info_list)
     scanners,latent_size = info_list
     for n_scanners in scanners:
+        print(f'Training {model} with {n_scanners} scanners on {classif_type} labels')
         test_size = 1 - (n_scanners/13)
         data_path = f'data/output/features_{model}.csv'
         output_path_mlp = f'classif_models/classifier_{model}_{n_scanners}scanners_mlp.h5'
