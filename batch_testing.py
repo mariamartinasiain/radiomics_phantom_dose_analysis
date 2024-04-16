3fb68bcd365dd8ccc3080e2a7958aab5efeaa014
JF <maestratijf@gmail.com>
2024-04-16 16:39:21 +0200

    testing

--- batch_testing.py
+++ batch_testing.py
@@ -21,8 +21,8 @@ def update_performance_file(model, scanners, mlp_accuracy, svm_accuracy, output_
         file.write(entry)
 
 configurations = {
-    'pyradiomics': ([1, 3, 5, 9], 86),
-    'swinunetr': ([1, 3, 5, 9], 3072)
+    'pyradiomics': ([1, 3, 5, 9], 20),
+    'swinunetr': ([1, 3, 5, 9], 20)
 }
 classif_type = 'roi_large'
 qmg = None#10
