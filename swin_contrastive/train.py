b4529f76f52ec2276601e42f38cdad9943702b91
JF <maestratijf@gmail.com>
2024-04-23 16:45:41 +0200

    no classification step

--- swin_contrastive/train.py
+++ swin_contrastive/train.py
@@ -322,7 +322,7 @@ def main():
     data_list = load_json(jsonpath)
     train_data, test_data = create_datasets(data_list)
     
-    train_dataset = SmartCacheDataset(data=train_data, transform=transforms,cache_rate=0.071,progress=True,num_init_workers=8, num_replace_workers=8)
+    train_dataset = SmartCacheDataset(data=train_data, transform=transforms,cache_rate=0.069,progress=True,num_init_workers=8, num_replace_workers=8)
     test_dataset = SmartCacheDataset(data=test_data, transform=transforms,cache_rate=0.15,progress=True,num_init_workers=8, num_replace_workers=8)
     
     train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True,collate_fn=custom_collate_fn, num_workers=4)
