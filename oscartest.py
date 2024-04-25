e6a435f87af1807301560f038a2a6c252d3f46c2
JF <maestratijf@gmail.com>
2024-04-25 17:05:44 +0200

    f

--- oscartest.py
+++ oscartest.py
@@ -94,8 +94,8 @@ def run_inference():
         image_tf = tf.transpose(input_tensor, [0, 2, 3, 4, 1])  # Reorder dimensions
         i = 0
         #iterating over every entry in the data
-        for batch in tqdm(range(len(dataset))):
-                                            
+        for _ in tqdm(range(len(dataset))):
+            batch = next(iter(dataload))                                 
             flattened_image = tf.reshape(image_tf, [-1, 131072])  # Flatten the tensor to match the input placeholder
             # Extract features using the reshaped tensor
             features = sess.run(feature_tensor, feed_dict={x: sess.run(flattened_image, feed_dict={input_tensor: batch["image"].numpy()}), keepProb: 1.0})
