c60d1506d7a0a0aaddee5819b22f860dfd452a8a
JF <maestratijf@gmail.com>
2024-05-12 06:34:58 +0200

    more testeting

--- swin_contrastive/swinunetr.py
+++ swin_contrastive/swinunetr.py
@@ -189,7 +189,7 @@ def get_model():
 
     weight = torch.load("model_final_weights2.pth")
     print("Loaded weight keys:", weight.keys())
-    model.load_from(weights=weight)
+    model.load_state_dict(weights=weight)
     model = model.to('cuda')
     print("Using pretrained self-supervied Swin UNETR backbone weights !")
     return model
