5bf0798a0fb0bc9ccdd76d6fabb7aa2fd48c741d
JF <maestratijf@gmail.com>
2024-05-23 17:26:02 +0200

    fix

--- swin_contrastive/swinunetr.py
+++ swin_contrastive/swinunetr.py
@@ -216,7 +216,7 @@ def run_inference(model,jsonpath = "./dataset_info_full_uncompressed_NAS.json"):
 
     datafiles = load_data(jsonpath)
     #dataset = SmartCacheDataset(data=datafiles, transform=transforms, cache_rate=0.009, progress=True, num_init_workers=8, num_replace_workers=8)
-    dataset = SmartCacheDataset(data=datafiles, transform=transforms,cache_rate=0.00049,progress=True,num_init_workers=8, num_replace_workers=8,replace_rate=0.2)
+    dataset = SmartCacheDataset(data=datafiles, transform=transforms,cache_rate=0.049,progress=True,num_init_workers=8, num_replace_workers=8,replace_rate=0.2)
     print("dataset length: ", len(datafiles))
     dataload = ThreadDataLoader(dataset, batch_size=1, collate_fn=custom_collate_fn)
     #qq chose comme testload = DataLoader(da.....
