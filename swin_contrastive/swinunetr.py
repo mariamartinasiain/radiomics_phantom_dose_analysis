814da96af337c1a7203203f7d2042ae7e960e7d3
JF <maestratijf@gmail.com>
2024-05-27 14:38:46 +0200

    repairing

--- swin_contrastive/swinunetr.py
+++ swin_contrastive/swinunetr.py
@@ -268,8 +268,9 @@ def run_inference(model,jsonpath = "./dataset_info_full_uncompressed_NAS_missing
             true_path = batch["roi_path"] 
             name = true_path[0]
             #remobing file path information and only keeping file name of the path
-            name = os.path.basename(name)
             print("Name",name)
+            name = os.path.basename(name)
+            
             nib.save(image, "uncompress_cropped/"+name)
             
             
