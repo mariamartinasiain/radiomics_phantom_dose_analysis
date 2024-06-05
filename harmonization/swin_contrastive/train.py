64d58a9ab2d14b7f4281cba0dafea70e5938833f
JF <maestratijf@gmail.com>
2024-06-05 14:23:04 +0200

    wrote loss ploting code

--- harmonization/swin_contrastive/train.py
+++ harmonization/swin_contrastive/train.py
@@ -459,6 +459,10 @@ def group_data(data_list, mode='scanner'):
     return np.array(group_ids)
 
 def create_datasets(data_list, test_size=0.2, seed=42):
+    
+    if test_size <= 0.00000001:
+        return data_list, []
+    
     groups = group_data(data_list, mode='scanner') 
     
     
