084dfc0b43f62e59cc218bbd2271e36c71b3dbe8
JF <maestratijf@gmail.com>
2024-06-26 14:09:45 +0200

    computing feature from new model trained without H2

--- harmonization/swin_contrastive/swinunetr.py
+++ harmonization/swin_contrastive/swinunetr.py
@@ -198,7 +198,7 @@ def get_model(target_size = (64, 64, 32)):
     ).to(device)
 
     #weight = torch.load("model_swinvit.pt")
-    weight = torch.load("paper_contrastive_{SCANNER_TYPE}_Harmonized_1mGy_IR - #10.pth")
+    weight = torch.load(f"paper_contrastive_{SCANNER_TYPE}_Harmonized_1mGy_IR - #10.pth")
     print("Loaded weight keys:", weight.keys())
     #model.load_from(weight)
     model.load_state_dict(weight)
@@ -237,7 +237,7 @@ def run_inference(model,jsonpath = "./dataset_info_cropped.json"):
     dataload = ThreadDataLoader(dataset, batch_size=1, collate_fn=custom_collate_fn)
     #qq chose comme testload = DataLoader(da.....
     slice_num = 15
-    with open("paper_contrastive_{SCANNER_TYPE}_features2.csv", "w", newline="") as csvfile:
+    with open(f"paper_contrastive_{SCANNER_TYPE}_features2.csv", "w", newline="") as csvfile:
         fieldnames = ["SeriesNumber", "deepfeatures", "ROI", "SeriesDescription", "ManufacturerModelName", "Manufacturer", "SliceThickness"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
