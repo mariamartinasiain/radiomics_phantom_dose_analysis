dd827ee5fff5e5b16a5c3bd2076714d11ec2b7f0
JF <maestratijf@gmail.com>
2024-06-26 13:13:37 +0200

    computing feature from new model trained without F1

--- harmonization/swin_contrastive/swinunetr.py
+++ harmonization/swin_contrastive/swinunetr.py
@@ -195,11 +195,11 @@ def get_model(target_size = (64, 64, 32)):
         use_checkpoint=True,
     ).to(device)
 
-    weight = torch.load("model_swinvit.pt")
-    #weight = torch.load("paper_contrastive_F1_Harmonized_1mGy_IR - #10.pth")
+    #weight = torch.load("model_swinvit.pt")
+    weight = torch.load("paper_contrastive_F1_Harmonized_1mGy_IR - #10.pth")
     print("Loaded weight keys:", weight.keys())
-    model.load_from(weight)
-    #model.load_state_dict(weight)
+    #model.load_from(weight)
+    model.load_state_dict(weight)
     model = model.to('cuda')
     print("Using pretrained self-supervied Swin UNETR backbone weights !")
     return model
@@ -235,7 +235,7 @@ def run_inference(model,jsonpath = "./dataset_info_cropped.json"):
     dataload = ThreadDataLoader(dataset, batch_size=1, collate_fn=custom_collate_fn)
     #qq chose comme testload = DataLoader(da.....
     slice_num = 15
-    with open("paper_contrastive__features.csv", "w", newline="") as csvfile:
+    with open("paper_contrastive_F1_features2.csv", "w", newline="") as csvfile:
         fieldnames = ["SeriesNumber", "deepfeatures", "ROI", "SeriesDescription", "ManufacturerModelName", "Manufacturer", "SliceThickness"]
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
