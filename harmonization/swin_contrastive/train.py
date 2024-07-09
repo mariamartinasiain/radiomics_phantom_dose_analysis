1031df078e6e456f265d6856a7238583f2172e20
JF <maestratijf@gmail.com>
2024-07-09 15:47:15 +0200

    might zork for random patch training

--- harmonization/swin_contrastive/train.py
+++ harmonization/swin_contrastive/train.py
@@ -786,7 +786,7 @@ def main():
 
     #PROBLEME DE REGISTRATION : resize ? as a qucik fix ?
 
-    jsonpath = "./light_dataset_info_10.json"
+    jsonpath = "./resized_light_dataset_info_10.json"
     data_list = load_data(jsonpath)
     train_data, test_data = create_datasets(data_list,test_size=0.00)
     model = get_model(target_size=(64, 64, 32))
