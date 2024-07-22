95306954068fd5660ee333c48f909f3d3273b5e4
jeanfe <maestratijf@gmail.com>
2024-07-22 13:43:14 +0200

    to crop the mask

--- crop.py
+++ crop.py
@@ -7,11 +7,24 @@ def crop_volume(input_path, output_path, crop_coords):
     reader.SetFileName(input_path)
     image = reader.Execute()
 
+    # Get image size
+    size = image.GetSize()
+    print(f"Original image size: {size}")
+
+    # Flip crop coordinates from [z_start, z_end, y_start, y_end, x_start, x_end]
+    # to [x_start, x_end, y_start, y_end, z_start, z_end]
+    flipped_coords = crop_coords[4:6] + crop_coords[2:4] + crop_coords[0:2]
+    
+    # Calculate new size
+    new_size = [flipped_coords[1] - flipped_coords[0], 
+                flipped_coords[3] - flipped_coords[2], 
+                flipped_coords[5] - flipped_coords[4]]
+    
+    print(f"Crop coordinates: {flipped_coords}")
+    print(f"New size: {new_size}")
+
     # Crop the image
-    size = [crop_coords[5] - crop_coords[4], 
-            crop_coords[3] - crop_coords[2], 
-            crop_coords[1] - crop_coords[0]]
-    cropped_image = sitk.Crop(image, crop_coords[::2], size)
+    cropped_image = sitk.Crop(image, flipped_coords[::2], new_size)
 
     # Write the cropped image
     writer = sitk.ImageFileWriter()
