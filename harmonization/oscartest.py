244bdd7da1bcbca5d69c925d7047b39b67e0af80
jeanfe <maestratijf@gmail.com>
2024-07-26 17:08:48 +0200

    trying to convert tensorflow model to torch model, test

--- harmonization/oscartest.py
+++ harmonization/oscartest.py
@@ -15,7 +15,14 @@ from monai.transforms import Compose, LoadImaged, EnsureChannelFirstd, ToTensord
 from harmonization.swin_contrastive.swinunetr import CropOnROId
 from monai.data import SmartCacheDataset, DataLoader,ThreadDataLoader
 import torch
-
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+    except RuntimeError as e:
+        print(e)
 def test():
     data_dir = 'roiBlocks'
     out_dir = 'deepLearn/'
