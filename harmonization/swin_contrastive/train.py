fe8e89a0ab86528269fc4e33b0b80cdd84429e53
jeanfe <maestratijf@gmail.com>
2024-07-29 17:51:37 +0200

    working tensorflow to pytorhch ?

--- harmonization/swin_contrastive/train.py
+++ harmonization/swin_contrastive/train.py
@@ -849,7 +849,7 @@ def main():
     optimizer = optim.AdamW(model.parameters(), lr=1e-4, weight_decay=0.005) 
     lr_scheduler = CosineAnnealingLR(optimizer, T_max=50, eta_min=1e-6)
     
-    trainer = Train(model, data_loader, optimizer, lr_scheduler, 100,dataset,contrastive_latentsize=768,savename="contrast_oscar.pth",ortho_reg=0.001)
+    trainer = Train(model, data_loader, optimizer, lr_scheduler, 100,dataset,contrastive_latentsize=2048,savename="contrast_oscar.pth",ortho_reg=0.001)
     trainer.train()
 
 def classify_cross_val(results, latents_t, labels_t, latents_v, labels_v, groups, lock):
